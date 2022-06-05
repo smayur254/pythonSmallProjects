89e13b5651d3df2a1e53afcaff9ee4ae8932e5cb
smayur254 <smayur25491@gmail.com>
2022-06-05 10:57:03 +0530

    right angle star pattern added

---
+++ right_triangle_start_pattern.py
@@ -0,0 +1,10 @@
+user_input = int(input("Lines for right angle start pattern: "))
+
+for i in range(0,user_input+1):
+    for j in range(0,i):
+        print("*", end=' ')
+
+    print()
+        
+
+
